ad259cda2ed9d3c51072523a86643af637ee523a
sidchopp <sidchopp@gmail.com>
2024-08-19 15:57:24 -0600

    created basic file structure, Home page, Footer and Heading RC

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bottom-0 left-0 z-20 w-full p-4 p-5">
+      <span className="text-sm sm:text-center">
+        © 2024 <span className="text-blue-500">Nutri Fruit™</span>. All Rights
+        Reserved.
+      </span>
+    </footer>
+  );
+};
+
+export { Footer };
---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+const Heading = (): React.ReactElement => {
+  return (
+    <h1 className="mb-4 pt-20 px-8 md:pt-24 text-5xl md:text-6xl lg:text-7xl font-extrabold leading-none tracking-tight  text-center">
+      Nutri <span className="text-blue-500">Fruit</span>
+    </h1>
+  );
+};
+
+export { Heading };
